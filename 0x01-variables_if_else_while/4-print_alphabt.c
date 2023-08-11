8beb0367e1214c0c11f57a52d6a80ecdfb8c7828
Ahmed Omer <ahmedomer695@gmail.com>
2023-08-11 15:14:38 +0200

    4-print_alphabet.c file created

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Alway 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+			putchar(c);
+		c = c + 1;
+	}
+	putchar('\n');
+}
