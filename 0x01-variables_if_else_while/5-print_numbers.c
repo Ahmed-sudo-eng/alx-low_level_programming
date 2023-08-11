618eb9ab3c17963a64db4fbd52a96182ddfc3851
Ahmed Omer <ahmedomer695@gmail.com>
2023-08-11 16:14:12 +0200

    5-print_numbers.c file created

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x;
+	x = 0;
+	while (x < 10)
+	{
+		printf("%d", x);
+		x = x + 1;
+	}
+	putchar(10); /* 10 = \n (new line) */
+	return (0);
+}
+
