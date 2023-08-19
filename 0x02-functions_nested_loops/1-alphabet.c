426991c614c04700ac85b0567fe1e0de218ca6c4
Ahmed Omer <ahmedomer695@gmail.com>
2023-08-19 13:05:04 +0200

    1-alphabet.c file modified

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,18 @@
 #include "main.h"
 
+/**
+ * print_alphabet - prints alphabet in lowercase
+ * Return: It has no return (void)
+ */
 void print_alphabet(void)
 {
 	char n;
-	
+
 	n = 97;
 	while (n <= 122)
 	{
 		_putchar(n);
 		n++;
 	}
+	_putchar('\n');
 }
