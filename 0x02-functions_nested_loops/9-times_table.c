33fba304b0c84839faeabeb03b7c77f375b5a0f1
Ahmed Omer <ahmedomer695@gmail.com>
2023-08-20 14:06:23 +0200

    9-times_table.c file modified

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,11 +19,31 @@ void times_table(void)
 		{
 			 n = i * j;
 			 x = n % 10;
-			 y = (n - x) / 10
-			_putchar(' ');
-			_putchar(' ');
-			if (
-			_putchar(',');
+			 y = (n - x) / 10;
+
+			 if (j == 0)
+			 {
+				 _putchar('0' + n);
+				 _putchar(',');
+			 }
+			 else
+			 {
+				 _putchar(' ');
+				if (n < 10)
+				{
+					_putchar(' ');
+					_putchar('0' + n);
+				}
+				else
+				{
+					_putchar('0' + y);
+					_putchar('0' + x);
+					}
+				if (j == 9)
+					continue;
+				else
+					_putchar(',');
+			 }
 		}
 		_putchar('\n');
 	}
