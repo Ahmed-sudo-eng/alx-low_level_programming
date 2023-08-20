c210815b674ddbaf98cbd97a6412a5c836acbe2d
Ahmed Omer <ahmedomer695@gmail.com>
2023-08-20 07:28:01 +0200

    7-print_last_digit.c file modified

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,6 +8,11 @@
  */
 int print_last_digit(int n)
 {
-	_putchar(n % 10);
-	return (n % 10);
+	int r;
+	int a;
+
+	r = n % 10;	/* r is the last digit of n */
+	a = r + 48;	/* convert r to ascii -a- */
+	_putchar(a);
+	return (r);
 }
