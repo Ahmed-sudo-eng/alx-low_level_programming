83556fac59816c09389eb13f83696f070997c989
Ahmed Omer <ahmedomer695@gmail.com>
2023-08-25 06:41:12 +0200

    1-strncat.c file created

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncat - a function that concatenates two strings depends on the number
+ * of bytes
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes
+ *
+ * Return: Destination string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+		i++;	/* get dest length */
+	j = 0;
+	while (j < n)
+	{
+		dest[i + j] = src[j];	/* add n bytes from src to dest */
+		j++;
+	}
+	return (dest);
+
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,1 +1,3 @@
 0-strcat.c: a function that concatenates two strings.
+
+1-strncat.c: a function that concatenates two strings depending on the number of bytes.
