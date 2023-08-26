3f9e24de91925f294d038d977523d4576f98e83b
Ahmed Omer <ahmedomer695@gmail.com>
2023-08-26 08:55:57 +0200

    3-print_numbers.c file created

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - a function that prints the numbers, from 0 to 9, followed by a new line
+ *
+ * Return: Nothing
+ */
+void print_numbers(void)
+{
+	unsigned char c;
+
+	for (c = 48; c <= 57; c++)
+		_putchar(c);
+	_putchar(10);
+}
