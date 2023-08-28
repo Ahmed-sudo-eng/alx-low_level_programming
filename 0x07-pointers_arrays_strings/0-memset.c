ebc4916d0e0c38f99cc845dc232fa65d376e6bc0
Ahmed Omer <ahmedomer695@gmail.com>
2023-08-28 08:42:51 +0200

    1-memcpy.c file created

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,6 +5,8 @@
  * @s: memory area
  * @b: constant byte
  * @n: how much to fill the memory area
+ *
+ * Return: a pointer to the memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
