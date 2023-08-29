708f68fdc2fd9634d91aeb0cd4c1dc886a02051a
Ahmed Omer <ahmedomer695@gmail.com>
2023-08-29 20:33:19 +0200

    100-ser_string.c file created

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,3 +4,4 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
