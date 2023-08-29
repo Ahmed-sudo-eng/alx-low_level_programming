b13f354c71e5f8b06d431970711be3e0ce013e4c
Ahmed Omer <ahmedomer695@gmail.com>
2023-08-29 12:18:00 +0200

    8-print_diagsums.c file created

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - a function that prints the sum of the two diagonals of 
+ * a square matrix of integers.
+ * @a: the array
+ * @size: the array size
+ *
+ * Return: Nothing
+ */
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int j;
+	int d1;
+	int d2;
+
+	i = 0;
+	d1 = 0;
+	while (i < size * size)
+	{
+		d1 = d1 + a[i];
+		i = i + size + 1;
+	}
+
+	j = size - 1;
+	d2 = 0;
+	while (j < (size * size) - 1)
+	{
+		d2 = d2 + a[j];
+		j = j + size - 1;
+	}
+	printf("%d, %d\n", d1, d2);
+}
+
+
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -5,3 +5,5 @@
 2-strchr.c: a function that locates a character in a string.
 
 7-print_chessboard.c: a function that prints the chessboard.
+
+8-print_diagsums.c: a function that prints the sum of the two diagonals of a square matrix of integers.
