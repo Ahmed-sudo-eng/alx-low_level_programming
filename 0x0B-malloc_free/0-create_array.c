280997d2fe0a225f771fad9ce3b8d2d80c5b5353
Ahmed Omer <ahmedomer695@gmail.com>
2023-09-04 10:51:32 +0200

    0x0B-malloc_free directory, main.h, README.md, 0-create_array.c files are created

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+
+/**
+ * create_array - a funtion that creates an array of chars, and intializes it
+ * with a specific char
+ * @size: the array size
+ * @c: char to fill the array with
+ *
+ * Return: a pointer to the array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *a;
+	unsigned int i;
+
+	if (size == 0)
+		return ('\0');
+	a = malloc(sizeof(char) * size);
+	for (i = 0; i < size; i++)
+		a[i] = c;
+	return (a);
+}
