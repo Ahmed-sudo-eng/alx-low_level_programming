1f044c045d5492321d296ffaeaad97ac65f51eb7
Ahmed Omer <ahmedomer695@gmail.com>
2023-09-04 11:34:08 +0200

    0-create_array.c file created

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,6 +16,8 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return ('\0');
 	a = malloc(sizeof(char) * size);
+	if (a == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 		a[i] = c;
 	return (a);
