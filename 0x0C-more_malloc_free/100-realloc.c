57618283c899acb96392e82267de07cfd5bf78b4
Ahmed Omer <ahmedomer695@gmail.com>
2023-09-07 19:05:02 +0200

    100-realloc.c file created

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,8 +15,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (ptr);
 	if (ptr == NULL)
 	{
-		ptr = malloc(new_size);
-		return (ptr);
+		pptr = malloc(new_size);
+		return (pptr);
 	}
 	if (new_size == 0 && ptr != NULL)
 	{
