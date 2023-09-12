0a957f5a8b8ed88421bf723735795c8f8afdc53e
Ahmed Omer <ahmedomer695@gmail.com>
2023-09-12 08:59:12 +0200

    5-free_dog.c file created

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - a function that frees dogs
+ * @d: a pointer to the dog
+ *
+ * Return: Nothing
+ */
+void free_dog(dog_t *d)
+{
+	free(d);
+}
