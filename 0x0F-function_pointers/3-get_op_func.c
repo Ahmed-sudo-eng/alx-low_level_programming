a78cbaf1ee644f468552bef1b7cb979cac4343d1
Ahmed Omer <ahmedomer695@gmail.com>
2023-09-13 22:43:34 +0200

    3-main.c, 3-calc.h and 3-get_op_func.c files are modified

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,4 +1,6 @@
 #include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
 #include <stddef.h>
 
 /**
@@ -9,24 +11,23 @@
  * Return: a pointer to the function that corresponds to the opreator given
  * as a parameter
  */
-int (*get_op_func(char *s))(int, int)
+int (*get_op_func(char s))(int, int)
 {
 	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
+		{'+', op_add},
+		{'-', op_sub},
+		{'*', op_mul},
+		{'/', op_div},
+		{'%', op_mod},
 	};
 	int i;
 
 	i = 0;
-	while (i < 6)
+	while (i < 5)
 	{
 		if (s == ops[i].op)
 			return (ops[i].f);
 		i++;
 	}
-	return (0);
+	return (NULL);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -7,10 +7,20 @@ int main(int argc, char *argv[])
 	int res;
 	op_t user_op;
 
+	if ((*argv[2] == '/' || *argv[2] == '%') && atoi(argv[3]) == 0)
+	{
+		puts("Error");
+		exit(100);
+	}
 	if (argc == 4)
 	{
-	user_op.op = argv[2];
-	user_op.f = get_op_func(user_op.op);
+	user_op.op = atoi(argv[2]);
+	user_op.f = get_op_func(*argv[2]);
+	if (user_op.f == NULL)
+	{
+		puts("Error");
+		exit(99);
+	}
 	res = user_op.f(atoi(argv[1]), atoi(argv[3]));
 	printf("%d\n", res);
 	}
