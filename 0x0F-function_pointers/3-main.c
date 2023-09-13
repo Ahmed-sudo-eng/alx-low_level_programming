539307fa8b78380a817b638276a7a7254d90d18a
Ahmed Omer <ahmedomer695@gmail.com>
2023-09-13 21:53:25 +0200

    3-main.c file modified

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -5,20 +5,14 @@
 int main(int argc, char *argv[])
 {
 	int res;
+	op_t user_op;
 
 	if (argc == 4)
 	{
-		if (*argv[2] == '+')
-			res = atoi(argv[1]) + atoi(argv[3]);
-		if (*argv[2] == '-')
-			res = atoi(argv[1]) - atoi(argv[3]);
-		if (*argv[2] == '*')
-			res = atoi(argv[1]) * atoi(argv[3]);
-		if (*argv[2] == '/')
-			res = atoi(argv[1]) / atoi(argv[3]);
-		if (*argv[2] == '%')
-			res = atoi(argv[1]) % atoi(argv[3]);
-		printf("%d\n", res);
+	user_op.op = argv[2];
+	user_op.f = get_op_func(user_op.op);
+	res = user_op.f(atoi(argv[1]), atoi(argv[3]));
+	printf("%d\n", res);
 	}
 	else
 	{
