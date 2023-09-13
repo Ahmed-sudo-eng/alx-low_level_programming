371f8ad56170c7546f82aa08e3e712869adbb2c1
Ahmed Omer <ahmedomer695@gmail.com>
2023-09-13 23:44:24 +0200

    100-main_opcodes.c file modified

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - Entry point
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: 0 in Success
+ */
 int main(int argc, char *argv[])
 {
 	if (atoi(argv[1]) < 0)
@@ -10,8 +17,8 @@ int main(int argc, char *argv[])
 	}
 	if (argc == 2)
 	{
-		printf("55 48 89 e5 48 83 ec 30 89 7d dc 48 89 75 d0 83 7d dc"
-				"02 74 14");
+		printf("55 48 89 e5 48 83 ec 30 89 7d dc 48 89 75 d0 83 7d dc");
+		printf("02 74 14\n");
 	}
 	else
 	{
