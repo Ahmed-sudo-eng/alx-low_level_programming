6ca547e37bce1ba4997217dcdb6739787af79181
Ahmed Omer <ahmedomer695@gmail.com>
2023-09-21 22:16:44 +0200

    0x12-singly_linked_lists directory, 0-print_list.c, README.md and lists.h files created

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - a function that prints all the elements of a list_t list
+ * @h: a pointer to the singly linked list
+ *
+ * Return: the number of nodes
+ */
+int print_list(const list_t *h)
+{
+	int i = 0;
+	const list_t *current = h;
+
+	while (current != NULL)
+	{
+		if (current->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", current->len , current->str);
+		current = current->next;
+		i++;
+	}
+	return (i);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+0-print_list.c: a function that prints all the elemnts of a list_t list.
