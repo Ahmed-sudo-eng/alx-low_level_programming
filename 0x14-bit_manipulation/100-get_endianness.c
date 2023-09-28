263c388a1288d002d4c0f3f0c90d0a64956b39a0
Ahmed Omer <ahmedomer695@gmail.com>
2023-09-28 22:11:25 +0200

    100-get_endianness.c file created

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * get_endianness - a function that checks the endianness
+ *
+ * Return: 0 if big endian, 1if littel endian
+ */
+int get_endianness(void)
+{
+	return (1);
+}
