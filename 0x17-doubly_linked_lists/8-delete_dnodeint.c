296b70e456f2e70700c8a4fc55f1442ac798d58b
Ahmed Omer <ahmedomer695@gmail.com>
2023-11-10 12:57:37 +0200

    8-delete_dnodeint.c file modified

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -14,7 +14,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *current = *head;
 	dlistint_t *nextNode = NULL;
 	dlistint_t *prevNode = NULL;
-	dlistint_t *tmp = NULL;
+	/*dlistint_t *tmp = NULL;*/
 	unsigned int i = 0;
 
 	if (*head == NULL)
