04d5d72897a38811fd543b4ded322d5b8998feb1
Ahmed Omer <ahmedomer695@gmail.com>
2023-12-01 11:00:25 +0200

    0-hash_table_create.c file was modified

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,6 +13,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *hashtable = (hash_table_t *)malloc(sizeof(hash_table_t));
 	hash_node_t *array = (hash_node_t *)calloc(size, sizeof(hash_node_t));
 
+	/* Make sure the memory was allocated succesfully */
+	if (hashtable == NULL)
+		return NULL;
+	if (array == NULL)
+		return NULL
+
 	hashtable->size = size;
 	hashtable->array = &array;
 
